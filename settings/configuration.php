774f5ba81195c1fa0e4efbc92cb791dc5e4c95d1
swomma <swomma@swomma.pl>
2016-07-05 13:45:42 +0200

    setting JSON on

--- settings/configuration.php
+++ settings/configuration.php
@@ -4,5 +4,5 @@
 // All of the settings defined here can be overridden in the /conf/config.php file.
 
 $Configuration["API"]["Expiration"] = strtotime("5 minutes", 0);
-$Configuration["API"]["AllowJSONP"] = false;
+$Configuration["API"]["AllowJSONP"] = true;
 $Configuration["API"]["AllowCORS"] = false;
